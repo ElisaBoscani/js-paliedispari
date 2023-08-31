89cc5685af7269c6661337825e06a214ae785024
Elisa Boscani <elisabosc90@gmail.com>
2023-08-31 17:32:07 +0200

    fixed the exercise

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,9 +1,10 @@
 /* Chiedere all’utente di inserire una parola 
 Creare una funzione per capire se la parola inserita è palindroma */
 const userWord = prompt("Scrivi una parola");
+let word = userWord.length;
 function palindroma() {
-  for (let i = 0; i < userWord.length; i++) {
-    if (userWord.length === userWord[-1 - i]) {
+  for (let i = 0; i < word; i++) {
+    if (userWord[i] === userWord[word - 1 - i]) {
       return true;
     } else {
       return false;
@@ -12,3 +13,8 @@ function palindroma() {
 }
 const palindromo = palindroma();
 console.log(palindromo);
+if (palindromo) {
+  console.log("è palindroma");
+} else {
+  console.log("non è palindroma");
+}
